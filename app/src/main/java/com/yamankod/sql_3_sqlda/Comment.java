43584e8ec35e9b4c3e5fe5f75487aea638d858bb
murat <muratymn.72@gmail.com>
2016-12-03 16:29:22 +0300

    v1

---
+++ app/src/main/java/com/yamankod/sql_3_sqlda/Comment.java
@@ -0,0 +1,29 @@
+package com.yamankod.sql_3_sqlda;
+
+public class Comment {
+	 private long id;
+	  private String comment;
+
+	  public long getId() {
+	    return id;
+	  }
+
+	  public void setId(long id) {
+	    this.id = id;
+	  }
+
+	  public String getComment() {
+	    return comment;
+	  }
+
+	  public void setComment(String comment) {
+	    this.comment = comment;
+	  }
+
+	  @Override
+	  public String toString() {
+	    return comment;
+	  }
+}
+
+
